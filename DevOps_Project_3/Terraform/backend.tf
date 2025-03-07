205ae994fd1e0b20bdbefa2edec6fce6e1fd390b
suryan70195 <118881595+suryan70195@users.noreply.github.com>
2025-03-07 18:47:24 +0530

    Update backend.tf

--- DevOps_Project_3/Terraform/backend.tf
+++ DevOps_Project_3/Terraform/backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket = "euran-terraform-statefile"
+    bucket = "chandu-terraform-statefile"
     key = "eks/ngg_cluster_name/statefile"
     region = "ap-south-1"
   }
