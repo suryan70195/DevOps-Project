26c9c8f9d7d631d78a13c24f2b932b407a1c03d8
suryan70195 <118881595+suryan70195@users.noreply.github.com>
2025-03-08 01:59:10 +0530

    Update main.tf

--- DevOps_Project_3/Terraform/main.tf
+++ DevOps_Project_3/Terraform/main.tf
@@ -84,7 +84,7 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
 resource "aws_eks_cluster" "myeks" {
     name = var.cluster_name
     role_arn = aws_iam_role.cluster_role.arn
-    version = "1.24"
+    version = "1.27"
     vpc_config {
         
         subnet_ids = data.aws_subnets.subnet_id.ids
