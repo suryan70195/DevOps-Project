e5a99d774b03c457dc06721a4c72a0d2e2163649
suryan70195 <118881595+suryan70195@users.noreply.github.com>
2025-03-08 12:57:43 +0530

    Update variables.tf

--- DevOps_Project_3/Terraform/variables.tf
+++ DevOps_Project_3/Terraform/variables.tf
@@ -3,18 +3,36 @@ variable "region" {
   default     = ""
 }
 
-variable "workstation_external_ip" {
-  type    = string
-  default = "0.0.0.0/0"
+variable "vpc_id" {
+  description = "VPC ID for the EKS cluster"
+  type        = string
 }
 
+
+variable "workstation_external_ip" {
+  type    = string
+  default = "0.0.0.0"
+}
+
+variable "eks_version" {
+  description = "Version of EKS cluster"
+  default     = "1.27"
+}
 
 variable "cluster_name" {
   description = "Name of the EKS cluster"
-  default     = ""
+  type        = string
+  default     = "my-eks-cluster"
 }
+
 
 variable "node_instance_type" {
   description = "EC2 instance type for EKS worker nodes"
   default     = "c5.xlarge"
 }
+
+variable "eks_ami_id" {
+  description = "Amazon Linux 2 AMI for EKS nodes"
+  default     = "ami-0ec98c2db7d0a924c"
+}
+
