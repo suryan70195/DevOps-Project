eb11adc597b3a3596078db6133adb840cd0eccc3
suryan70195 <118881595+suryan70195@users.noreply.github.com>
2025-03-09 00:34:03 +0530

    Update main.tf

--- DevOps_Project_3/Terraform/main.tf
+++ DevOps_Project_3/Terraform/main.tf
@@ -59,6 +59,41 @@ resource "aws_eks_cluster" "myeks" {
     security_group_ids      = [aws_security_group.EKS_SG.id]
   }
 }
+resource "aws_iam_role" "eks_node_role" {
+  name = "${var.cluster_name}-terraform-eks-eks_node_role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_role-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_role-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_role-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+
 
 resource "aws_eks_node_group" "mynode_node" {
   cluster_name    = aws_eks_cluster.myeks.name
